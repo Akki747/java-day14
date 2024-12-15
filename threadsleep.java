c5dd4f3ea9079ad8b833901dcee19f687bc63cad
Akki747 <bairapuakshaya@gmail.com>
2024-12-15 12:20:30 +0530

    Add files via upload

---
+++ threadsleep.java
@@ -0,0 +1,12 @@
+public class threadsleep{
+    public static void main(String args[]){
+        System.out.println("this this what is this.........?");
+        try{
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException e){
+            System.out.println("anna nenu Akshaya ni");
+        }
+            System.out.println("life is sooo happy with you.............. ");
+    }
+}
